8ecf9092abc684cdf870f61f184a43273ac252e3
boubamg <bouba.maiga99@gmail.com>
2020-08-23 17:18:34 +0200

    adding Header to resqponse

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// Request authorization
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
+app.use((req,res) => {
+    res.json({mesage : 'Votre requête a bien été reçue.'})
+});
 
 module.exports = app;
