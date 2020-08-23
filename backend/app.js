7436b829325d6ac12fc6080046d6e7caad183210
boubamg <bouba.maiga99@gmail.com>
2020-08-24 00:08:37 +0200

    Installation of mongoose and  Connection to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Database Connection
+mongoose.connect(
+    "mongodb+srv://boubamg9:Annete215@cluster0.i0ghu.mongodb.net/Cluster0?retryWrites=true&w=majority", 
+{ useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => console.log("Connexion à mongoDB réussie"))
+.catch(() => console.log("Echec de connexion à mongoDB"));
+
 
 // Request authorization
 app.use((req, res, next) => {
@@ -8,7 +17,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
 app.use((req,res) => {
     res.json({mesage : 'Votre requête a bien été reçue.'})
