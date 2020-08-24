29f692797d418b81dc9096eb463e34cace429c8e
boubamg <bouba.maiga99@gmail.com>
2020-08-24 03:57:46 +0200

    creation of all sauces route, one sauce route , and add sauce route

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+// import models
+const Sauce = require('./models/Sauce');
+
 const app = express();
 
 // Database Connection
@@ -21,6 +24,32 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+
+// Sauce Route
+
+app.use('/api/sauces', (req, res) => {
+    Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(err => res.status(400).json({ err }));
+});
+
+app.get('/api/sauces/:id', (req, res) => {
+    Sauce.findOne({_id: req.params.id})
+    .then(sauce => res.status(200).json(sauce))
+    .catch(err => res.status(400).json({ err }));
+});
+
+app.post('/api/sauces', (req, res) => {
+    const sauceParsed = JSON.parse(req.body.sauce);
+    delete sauceParsed._id;
+    const sauce = new Sauce({
+        ...sauceParsed,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    sauce.save()
+    .then(() => res.status(201).json({ message: 'Sauce ajoutée'}))
+    .catch(err => res.status(400).json({err}));
+});
 
 app.use((req,res) => {
     res.json({mesage : 'Votre requête a bien été reçue.'})
