cd902eaf03c1dcc92d139c5ee593704ad9e39e94
boubamg <bouba.maiga99@gmail.com>
2020-08-27 05:08:56 +0200

    adding middleware on all routes

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const sauceController = require("../controllers/sauce")
 
-router.get('/', sauceController.getAllSauce);
-router.post('/', sauceController.createSauce);
-router.get('/:id', sauceController.getOneSauce);
-router.put('/:id', sauceController.modifySauce);
-router.delete('/:id', sauceController.deleteSauce);
+router.get('/', auth, sauceController.getAllSauce);
+router.post('/', auth, sauceController.createSauce);
+router.get('/:id', auth, sauceController.getOneSauce);
+router.put('/:id', auth, sauceController.modifySauce);
+router.delete('/:id', auth, sauceController.deleteSauce);
 
 module.exports = router;
