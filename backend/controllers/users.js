1bcc0de4848364a665358aad4ada1d0cb27beb2c
boubamg <bouba.maiga99@gmail.com>
2020-08-27 04:55:43 +0200

    install jsonwebtoken and use it to create authentification token

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+
 exports.signup = (req, res) => {
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
